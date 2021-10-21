d42e614303741233288796614183aae4d2a96ca1
hosamsaleh1 <mr.hosamsaleh@yahoo.com>
2021-10-21 12:42:03 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AboutComponent } from './views/about/about.component';
+import { UsersComponent } from './views/users/users.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { PostsComponent } from './views/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    UsersComponent,
+    ProfileComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
